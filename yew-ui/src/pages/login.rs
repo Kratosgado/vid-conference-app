980ac3dfe05f91006f3ac16adb582d91ccb2fee8
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-19 21:13:45 +0000

    style login screen

--- yew-ui/src/pages/login.rs
+++ yew-ui/src/pages/login.rs
@@ -2,10 +2,11 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 use web_sys::HtmlInputElement;
 
-use crate::router::Route;
+use crate::route::Route;
+const TEXT_INPUT_CLASSES: &str = "rounded-md mx-2 p-2 text-black required:ring-2 required:ring-red-500 required:valid:ring-2 required:valid:ring-green-500";
 
 #[function_component(Login)]
-pub fn home() -> Html {
+pub fn login() -> Html {
     let navigator = use_navigator().unwrap();
 
     let username_ref = use_node_ref();
@@ -27,21 +28,36 @@ pub fn home() -> Html {
     };
 
     html! {
-        <div>
-            <h1>{ "Login" }</h1>
+        <div class="flex justify-center items-center content-center flex-col m-auto">
+            <div class="flex items-center flex-col">
+                <h1>{ "Login" }</h1>
+            </div> 
             <form onsubmit={onsubmit}>
-                <label>{ "Username" }</label>
-                <input ref={username_ref} type="text" id="username" />
-                <input 
-                ref={password_ref}
-                 label="Password"
-                  type="password_ref" 
-                  placeholder="Password"
-                  required={true}
-                  pattern="^[a-zA-Z0-9]{8,}$"
-                  id="password_ref" />
-                <input type="submit" value="LOGIN"/>
-            </form>
+                    <div class="py-4">
+                        <input 
+                        class={TEXT_INPUT_CLASSES}
+                        ref={username_ref} 
+                        type="text"
+                        required={true}
+                        placeholder="Username"
+                        pattern="^[a-zA-Z0-9]{6,}$"
+                         id="username" 
+                    />
+                    <input 
+                        ref={password_ref}
+                        class={TEXT_INPUT_CLASSES}
+                        label="Password"
+                        type="password_ref" 
+                        placeholder="Password"
+                        required={true}
+                        pattern="^[a-zA-Z0-9]{8,}$"
+                        id="password"
+                    />
+                </div>
+
+                    <input type="submit" value="LOGIN" class="py-2 px-4 pointer bg-yew-blue rounded-md w-full cursor-pointer" 
+                    />
+                </form>
         </div>
     }
 }
