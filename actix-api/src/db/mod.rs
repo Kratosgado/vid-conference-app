2ca9ea53a679dc7aabd33ac924071ef6acd4740c
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-21 21:35:48 +0000

    setup http server

---
+++ actix-api/src/db/mod.rs
@@ -0,0 +1,6 @@
+use diesel::{pg::PgConnection, Connection};
+
+pub fn establish_connection() -> PgConnection {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer};
+use dotenvy::dotenv;
+use std::env::var;
+use std::io;
+
+pub mod db;
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let host = var("HOST").expect("HOST must be set");
+    let port = var("ACTIX_PORT")
+        .expect("ACTIX_PORT must be set")
+        .parse()
+        .unwrap();
+    HttpServer::new(move || App::new().service(hello))
+        .bind((host, port))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok().body("Hello world!")
 }
