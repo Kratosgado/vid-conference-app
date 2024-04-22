1a01214d4fc52b5f662a1b68d5f1a0cf7c99602f
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-22 22:32:10 +0000

    add logger

--- actix-api/src/db/users/service.rs
+++ actix-api/src/db/users/service.rs
@@ -1,13 +1,14 @@
+use crate::DbConn;
+
 use super::util::SignUpUser;
 use actix_web::HttpResponse;
 use diesel::RunQueryDsl;
 
-
-pub async fn sign_up(sign_up_data: SignUpUser) -> HttpResponse {
+pub async fn sign_up(conn: &mut DbConn, sign_up_data: SignUpUser) -> HttpResponse {
     use crate::schema::users;
 
-    let conn = &mut crate::db::establish_connection();
-    let res = diesel::insert_into(users::table).values(&sign_up_data)
+    let res = diesel::insert_into(users::table)
+        .values(&sign_up_data)
         .execute(conn);
 
     match res {
