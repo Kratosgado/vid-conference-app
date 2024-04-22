c212c9794d6bd87453b17da1bf6130d8f6d34939
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-22 22:32:10 +0000

    add logger

--- actix-api/src/db/users/controller.rs
+++ actix-api/src/db/users/controller.rs
@@ -1,12 +1,12 @@
+use crate::{DbConn, DbPool};
+
 use super::{service, util::SignUpUser};
 use actix_web::{post, web, HttpResponse};
 
 #[post("/signup")]
-pub async fn sign_up(req: web::Json<SignUpUser>) -> HttpResponse {
-    service::sign_up(SignUpUser {
-        username: req.username.clone(),
-        email: req.email.clone(),
-        password: req.password.clone(),
-    })
-    .await
+pub async fn sign_up(pool: web::Data<DbPool>, req: web::Json<SignUpUser>) -> HttpResponse {
+    log::info!("signing up user");
+    let mut conn: DbConn = pool.get().expect("couldn't get db connection from pool");
+
+    service::sign_up(&mut conn, req.into_inner()).await
 }
--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -1,28 +1,45 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer};
-use dotenvy::dotenv;
-use std::env::var;
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer};
+use diesel::{pg, r2d2, PgConnection};
 use std::io;
 
 pub mod db;
 pub mod schema;
 
+pub type DbPool = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
+pub type DbConn = diesel::r2d2::PooledConnection<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
+type DbManager = diesel::r2d2::ConnectionManager<diesel::PgConnection>;
+
 #[actix_web::main]
-async fn main() -> io::Result<()> {
-    dotenv().ok();
-    let host = var("HOST").expect("HOST must be set");
-    let port = var("ACTIX_PORT")
+async fn main() {
+    std::env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+
+    dotenvy::dotenv().ok();
+    let host = std::env::var("HOST").expect("HOST must be set");
+    let port: u16 = std::env::var("ACTIX_PORT")
         .expect("ACTIX_PORT must be set")
         .parse()
         .unwrap();
-    
-    HttpServer::new(move || {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager: DbManager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    let server = HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
             .service(hello)
-            .service(web::scope("/users").configure(db::users::user_config))
+            .service(web::scope("users").configure(db::users::user_config))
+            .default_service(web::route().to(|| HttpResponse::NotFound()))
     })
-    .bind((host, port))?
-    .run()
-    .await
+    .bind((host.clone(), port.clone()))
+    .unwrap();
+    log::info!("Starting server at http://{}:{}", host, port);
+
+    server.run().await.expect("Failed to start server");
 }
 
 #[get("/")]
