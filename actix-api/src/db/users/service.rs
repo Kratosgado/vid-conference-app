0a3d3a7acd5beb98f0a0700b50d02350cdaedcd0
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-23 22:17:20 +0000

    refactor codes

--- actix-api/src/db/users/service.rs
+++ actix-api/src/db/users/service.rs
@@ -9,12 +9,11 @@ use diesel::{delete, prelude::*};
 use crate::schema::users::dsl::*;
 
 
-pub async fn sign_up(conn: &mut DbConn, sign_up_data: SignUpUser) -> HttpResponse {
+pub async fn sign_up(pool: &web::Data<DbPool>, sign_up_data: SignUpUser) -> HttpResponse {
 
     log::info!("creating user with data: {:?}", sign_up_data.clone());
-    // let res = diesel::insert_into(users::table())
-    //     .values(&sign_up_data)
-    //     .execute(conn);
+    
+
     let (password_hash, salt_str) = crate::db::auth::hash_password(&sign_up_data.password);
 
     let new_user = User {
@@ -25,7 +24,9 @@ pub async fn sign_up(conn: &mut DbConn, sign_up_data: SignUpUser) -> HttpRespons
         salt: salt_str,
     };
 
-    let res = diesel::insert_into(users).values(&new_user).execute(conn);
+    let mut conn: DbConn = pool.get().unwrap();
+
+    let res = diesel::insert_into(users).values(&new_user).execute(&mut conn);
 
     match res {
         Ok(_) => {
@@ -40,12 +41,14 @@ pub async fn sign_up(conn: &mut DbConn, sign_up_data: SignUpUser) -> HttpRespons
 }
 
 // create function for login
-pub async fn login(conn: &mut DbConn, login_data: LoginUser) -> HttpResponse {
+pub async fn login(pool: &web::Data<DbPool>, login_data: LoginUser) -> HttpResponse {
 
     log::info!("finding user with email: {}", login_data.email.clone());
+    let mut conn: DbConn = pool.get().unwrap();
+
     let res: Result<User, _> = users
         .filter(email.eq(&login_data.email))
-        .first::<User>(conn);
+        .first::<User>(&mut conn);
 
     match res {
         Ok(user) => {
@@ -66,10 +69,11 @@ pub async fn login(conn: &mut DbConn, login_data: LoginUser) -> HttpResponse {
     }
 }
 
-pub async fn get_users(conn: &mut DbConn) -> HttpResponse {
+pub async fn get_users(pool: &web::Data<DbPool>) -> HttpResponse {
+    let mut conn: DbConn = pool.get().unwrap();
 
     log::info!("getting all users");
-    let res = users.load::<User>(conn);
+    let res = users.load::<User>(&mut conn);
 
     match res {
         Ok(rusers) => {
@@ -83,8 +87,10 @@ pub async fn get_users(conn: &mut DbConn) -> HttpResponse {
     }
 }
 
-pub async fn get_user_by_id(conn: &mut DbConn, user_id: String) -> HttpResponse {
-    let user = users.find(user_id).get_result::<User>(conn);
+pub async fn get_user_by_id(pool: &web::Data<DbPool>, user_id: String) -> HttpResponse {
+    let mut conn: DbConn = pool.get().unwrap();
+
+    let user = users.find(user_id).get_result::<User>(&mut conn);
     match user {
         Ok(user ) => {
             log::info!("user found");
@@ -97,7 +103,7 @@ pub async fn get_user_by_id(conn: &mut DbConn, user_id: String) -> HttpResponse 
     }
 }
 
-pub async fn delete_user(pool: web::Data<DbPool>, user_id: String) -> HttpResponse {
+pub async fn delete_user(pool: &web::Data<DbPool>, user_id: String) -> HttpResponse {
 
     let mut conn: DbConn = pool.get().unwrap();
 
