61e4da996eefb7ee849ee9778f04b207a6722cf5
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-23 22:17:20 +0000

    refactor codes

--- actix-api/src/db/users/controller.rs
+++ actix-api/src/db/users/controller.rs
@@ -9,33 +9,28 @@ use actix_web::{delete, get, post, web, HttpResponse};
 #[post("/signup")]
 pub async fn sign_up(pool: web::Data<DbPool>, req: web::Json<SignUpUser>) -> HttpResponse {
     log::info!("signing up user");
-    let mut conn: DbConn = pool.get().expect("couldn't get db connection from pool");
 
-    service::sign_up(&mut conn, req.into_inner()).await
+    service::sign_up(&pool, req.into_inner()).await
 }
 
 #[post("/login")]
 pub async fn login(pool: web::Data<DbPool>, req: web::Json<LoginUser>) -> HttpResponse {
     log::info!("logging in user");
-    let mut conn: DbConn = pool.get().expect("couldn't get db connection from pool");
-
-    service::login(&mut conn, req.into_inner()).await
+    service::login(&pool, req.into_inner()).await
 }
 
 #[get("/")]
 pub async fn get_users(pool: web::Data<DbPool>) -> HttpResponse {
     log::info!("getting all users");
 
-    let mut conn: DbConn = pool.get().expect("couldn't get db connection from pool");
-    service::get_users(&mut conn).await
+    service::get_users(&pool).await
 }
 
 #[get("/{id}")]
 pub async fn get_user(pool: web::Data<DbPool>, user_id: web::Path<String>) -> HttpResponse {
     log::info!("getting user by user id: {}", user_id);
-    let mut conn: DbConn = pool.get().expect("couldn't get db connection from pool");
 
-    service::get_user_by_id(&mut conn, user_id.into_inner()).await
+    service::get_user_by_id(&pool, user_id.into_inner()).await
 }
 
 // #[put("/{id}")]
@@ -53,5 +48,5 @@ pub async fn get_user(pool: web::Data<DbPool>, user_id: web::Path<String>) -> Ht
 #[delete("/{id}")]
 pub async fn delete_user(pool: web::Data<DbPool>, user_id: web::Path<String>) -> HttpResponse {
     log::info!("deleting user by id: {:?}", user_id);
-    service::delete_user(pool, user_id.into_inner()).await
+    service::delete_user(&pool, user_id.into_inner()).await
 }
