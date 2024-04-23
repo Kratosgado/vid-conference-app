329d500c884138fe85060a02d7d1d48f477440eb
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-23 14:45:23 +0000

    add password hash

--- actix-api/migrations/2024-04-23-120035_users/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
